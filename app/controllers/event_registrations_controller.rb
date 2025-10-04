7caf4f491d732efc445ab88b2b2d53a944d5aebd
RJ993 <196080894+RJ993@users.noreply.github.com>
2025-10-04 13:30:04 -0400

    Add strong params to event registration

--- app/controllers/event_registrations_controller.rb
+++ app/controllers/event_registrations_controller.rb
@@ -2,8 +2,7 @@ class EventRegistrationsController < ApplicationController
   before_action :authenticate_user!
   def create
     @event = Event.find(params[:event_id])
-    @event_registration = @event.event_registrations.build(attendee: current_user)
-    # @event_registration = @event.event_registrations.build(reg_params)
+    @event_registration = @event.event_registrations.build(reg_params)
 
     if @event_registration.save
       redirect_to @event
@@ -12,12 +11,6 @@ class EventRegistrationsController < ApplicationController
       flash.now[:alert] = "Something is not right."
       redirect_to @event
     end
-
-    # private
-
-    # def reg_params
-    # params.expect(event_registration: [ attendee_id: current_user, relevant_event_id: @event ])
-    # end
   end
 
 
@@ -33,5 +26,11 @@ class EventRegistrationsController < ApplicationController
       flash.now[:alert] = "Something is not right."
       redirect_to @event
     end
+  end
+
+  private
+
+  def reg_params
+      params.expect(event_registration: [ :attendee_id, :relevant_event_id, :status ])
   end
 end
